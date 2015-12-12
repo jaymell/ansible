cc8bab82548df020c7ab984b53d689ace4693aa8
james <jamesmlucas@gmail.com>
2015-12-12 13:21:00 -0600

    added roles for gcc and kernel source/header install and accompanying playbook

--- make_role_skel.sh
+++ make_role_skel.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-read -p "Enter the name of the new role: " role
+if [[ -z $1 ]]; then
+	read -p "Enter the name of the new role: " role
+else
+	role=$1
+fi
 mkdir roles/$role
 for i in files handlers tasks templates vars; do mkdir roles/$role/$i; done
