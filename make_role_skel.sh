41eaae915ba6878fb9474c4022b68385d1f20e55
James Lucas <lucjam@rac.raccorp.corp>
2015-12-29 10:35:51 -0600

    make_role_skel subdirs now match ansible docs

--- make_role_skel.sh
+++ make_role_skel.sh
@@ -6,4 +6,4 @@ else
 	role=$1
 fi
 mkdir -p roles/$role
-for i in files handlers tasks templates vars defaults; do mkdir roles/$role/$i; done
+for i in files handlers tasks templates vars defaults meta; do mkdir roles/$role/$i; done
