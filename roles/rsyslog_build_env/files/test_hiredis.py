839913fbe787405a96f63bf2e6123920722ac0fd
james <jamesmlucas@gmail.com>
2016-02-27 15:41:32 -0600

    debian rsyslog tests working

--- roles/rsyslog_build_env/files/test_hiredis.py
+++ roles/rsyslog_build_env/files/test_hiredis.py
@@ -3,21 +3,23 @@
 import redis
 import syslog
 import time
+import sys
 
 r = redis.StrictRedis()
 p = r.pubsub()
 p.psubscribe('rsyslog_*')
-
-syslog.syslog(syslog.LOG_CRIT, "OH FUCK")
+MESSAGE = 'OH FUCK'
+syslog.syslog(syslog.LOG_CRIT, MESSAGE)
 
 # why not:
 time.sleep(1)
 
 msg = p.get_message()
 while msg:
-    # should see "OH FUCK", need to
-    # pass random string then attempt
-    # to regex it and set exit code
-    # accordingly:
-    print(msg)
-    msg = p.get_message()
+	if msg['data']:
+		if isinstance(msg['data'], basestring):
+		    if MESSAGE in msg['data']:
+		    	sys.exit(0)
+	msg = p.get_message()
+
+sys.exit(1)
